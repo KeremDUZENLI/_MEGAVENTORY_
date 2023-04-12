bf71d5a898955d367787589ccb8464675df8ec53
kerem.duzenli <krmdznl97@gmail.com>
2023-04-12 14:53:12 -0300

    build: repository-service-controller fields are created

--- main.go
+++ main.go
@@ -5,8 +5,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+// variables
+var (
+	URL = "https://api.megaventory.com/v2017a/"
+
+	GET = "json/reply/ProductGet?APIKEY="
+	POS = "SupplierClient/SupplierClientUpdate"
+
+	KEY = "8ccd0b3378ef30a5@m140829"
+
+	HOST = ":8080"
+)
+
+var requestBody = bytes.NewBufferString(`{
+	"APIKEY": "8ccd0b3378ef30a5@m140829",
+	"mvSupplierClient": {
+	  "SupplierClientID": 0,
+	  "SupplierClientType": "Client33",
+	  "SupplierClientName": "My dummy client33"
+	},
+	"mvRecordAction": "Insert"
+}`)
+
+var productList ProductList
+var supplierClientList SupplierClientList
+
+// model
 type ProductCost struct {
 	CompanyName         string `json:"CompanyName"`
 	CompanyCurrencyCode string `json:"CompanyCurrencyCode"`
@@ -33,54 +60,84 @@ type SupplierClientList struct {
 	MvSupplierClient SupplierClient `json:"mvSupplierClient"`
 }
 
+// repository
+func GetProducts() *http.Response {
+	response, err := http.Get(URL + GET + KEY)
+	if err != nil {
+		fmt.Println("Get error: ", err)
+	}
+
+	return response
+}
+
+func PostProducts() *http.Request {
+	response, err := http.NewRequest("POST", URL+POS, requestBody)
+	if err != nil {
+		fmt.Println("Post error: ", err)
+	}
+
+	return response
+}
+
+// service
+func GetProductsService() ProductList {
+	results := GetProducts()
+
+	err := json.NewDecoder(results.Body).Decode(&productList)
+	if err != nil {
+		fmt.Println("responseGet is not parsed: ", err)
+	}
+
+	return productList
+}
+
+func PostProductsService() SupplierClientList {
+	results := PostProducts()
+
+	err := json.NewDecoder(results.Body).Decode(&supplierClientList)
+	if err != nil {
+		fmt.Println("responsePost is not parsed: ", err)
+	}
+
+	return supplierClientList
+}
+
+// controller
+func GetProductsController(w http.ResponseWriter, r *http.Request) {
+	results := GetProductsService()
+	fmt.Fprintf(w, convertStringGet(results))
+}
+
+func PostProductsController(w http.ResponseWriter, r *http.Request) {
+	results := PostProductsService()
+	fmt.Fprintf(w, convertStringPost(results))
+}
+
+func convertStringGet(listeGet ProductList) string {
+	var str strings.Builder
+
+	for _, v := range listeGet.MvProducts {
+		str.WriteString(fmt.Sprintf("%v\n", v))
+	}
+
+	return str.String()
+}
+
+func convertStringPost(listePost SupplierClientList) string {
+	var str strings.Builder
+
+	str.WriteString(fmt.Sprintf("%v\n", listePost.MvSupplierClient))
+
+	return str.String()
+}
+
+// router
+func Run(host string) {
+	http.HandleFunc("/get", GetProductsController)
+	http.HandleFunc("/post", PostProductsController)
+	http.ListenAndServe(host, nil)
+}
+
 func main() {
-	urlGet := "https://api.megaventory.com/v2017a/json/reply/ProductGet?APIKEY=8ccd0b3378ef30a5@m140829"
-	urlPost := "https://api.megaventory.com/v2017a/SupplierClient/SupplierClientUpdate"
-
-	requestBody := bytes.NewBufferString(`{
-        "APIKEY": "8ccd0b3378ef30a5@m140829",
-        "mvSupplierClient": {
-          "SupplierClientID": 0,
-          "SupplierClientType": "Client25",
-          "SupplierClientName": "My dummy client25"
-        },
-        "mvRecordAction": "Insert"
-      }`)
-
-	// repository
-	responseGet, errGet := http.Get(urlGet)
-	if errGet != nil {
-		fmt.Println("Get error: ", errGet)
-		return
-	}
-	defer responseGet.Body.Close()
-
-	responsePost, errPost := http.Post(urlPost, "application/json", requestBody)
-	if errPost != nil {
-		fmt.Println("Post error: ", errPost)
-	}
-	defer responsePost.Body.Close()
-
-	// service
-	var productList ProductList
-	errG := json.NewDecoder(responseGet.Body).Decode(&productList)
-	if errG != nil {
-		fmt.Println("responseGet is not parsed: ", errG)
-		return
-	}
-
-	var supplierClientList SupplierClientList
-	errP := json.NewDecoder(responsePost.Body).Decode(&supplierClientList)
-	if errP != nil {
-		fmt.Println("responsePost is not parsed: ", errP)
-	}
-
-	// controller
-	// for _, product := range productList.MvProducts {
-	// 	fmt.Printf("ProductID: %v   ProductType: %v   ProductUnitCost: %v\n",
-	// 		product.ProductID, product.ProductType, product.ProductUnitCost)
-	// }
-
-	fmt.Println(responseGet.Header.Get("Content-Type"))
-	fmt.Println(responsePost.Header.Get("Content-Type"))
+	Run(HOST)
 }
